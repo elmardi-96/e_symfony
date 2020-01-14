80220d80fead4d53f9c74a139d5f2bdba92dbf9e
elmardi-96 <55544949+elmardi-96@users.noreply.github.com>
2020-01-14 03:02:11 +0100

    first commit

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Student;
+use App\Form\StudentType;
+use App\Repository\StudentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * @Route("/student")
+ */
+class StudentController extends AbstractController
+{
+    /**
+     * @Route("/", name="student_index", methods={"GET"})
+     */
+    public function index(StudentRepository $studentRepository): Response
+    {
+        return $this->render('student/index.html.twig', [
+            'students' => $studentRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="student_new", methods={"GET","POST"})
+     */
+    public function new(Request $request,ValidatorInterface $validator): Response
+    {
+       
+        $student = new Student();
+        $errors = $validator->validate($student);
+        $form = $this->createForm(StudentType::class, $student);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if (count($errors) > 0) {
+                return $this->render('student/new.html.twig', [
+                    'errors' => $errors,
+                ]);
+            }else{
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($student);
+                $entityManager->flush();
+                return $this->redirectToRoute('student_index');
+            }
+        }
+        
+        return $this->render('student/new.html.twig', [
+            'student' => $student,
+            'form' => $form->createView(),
+        ]);
+        
+
+        
+    }
+
+    /**
+     * @Route("/{id}", name="student_show", methods={"GET"})
+     */
+    public function show(Student $student): Response
+    {
+        return $this->render('student/show.html.twig', [
+            'student' => $student,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="student_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Student $student): Response
+    {
+        $form = $this->createForm(StudentType::class, $student);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('student_index');
+        }
+
+        return $this->render('student/edit.html.twig', [
+            'student' => $student,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="student_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Student $student): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$student->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($student);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('student_index');
+    }
+}
---
+++ src/Entity/Student.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\StudentRepository")
+ */
+class Student
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\Length(
+     *      min = 10,
+     *      max = 10,
+     *      minMessage = "Your first name must be at least {{ limit }} characters long",
+     *      maxMessage = "Your first name cannot be longer than {{ limit }} characters"
+     * )
+     */
+    private $CNE;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\Regex(
+     *     "/^[a-zA-Z]+ {1}[a-zA-Z]+/",
+     *     message="the name is incorrect"
+     * )
+     */
+    private $Nom;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_naissance;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $adresse;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCNE(): ?string
+    {
+        return $this->CNE;
+    }
+
+    public function setCNE(string $CNE): self
+    {
+        $this->CNE = $CNE;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->date_naissance;
+    }
+
+    public function setDateNaissance(\DateTimeInterface $date_naissance): self
+    {
+        $this->date_naissance = $date_naissance;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(?string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+}
